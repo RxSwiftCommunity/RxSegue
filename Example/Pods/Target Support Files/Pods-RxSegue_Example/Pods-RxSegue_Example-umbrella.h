2ac15041c632763630ff7d918d9d01a5ae377ea7
sergdort <sergdort@gmail.com>
2016-03-19 15:04:54 +0200

    Removed /Example/Pods/ from .gitignore

---
+++ Example/Pods/Target Support Files/Pods-RxSegue_Example/Pods-RxSegue_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RxSegue_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RxSegue_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/RxSegue/RxSegue-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxSegue : NSObject
+@end
+@implementation PodsDummy_RxSegue
+@end
